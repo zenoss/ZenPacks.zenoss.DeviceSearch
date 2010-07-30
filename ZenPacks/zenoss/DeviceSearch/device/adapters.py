74f0cff6ea707129a976cfbf8ff0e67a48e31185
mray <mray@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-07-30 18:31:14 +0000

    Cleaned up Copyright headers reviewed by jplouis
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.DeviceSearch@35157 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/DeviceSearch/device/adapters.py
+++ ZenPacks/zenoss/DeviceSearch/device/adapters.py
@@ -1,10 +1,15 @@
-######################################################################
+###########################################################################
 #
-# Copyright 2010 Zenoss, Inc.  All Rights Reserved.
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2010, Zenoss Inc.
 #
-######################################################################
-
-
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
 from zope.component import adapts
 from zope.interface import implements
 from Products.AdvancedQuery import MatchGlob, And, Or, Eq, In, RankByQueries_Max
