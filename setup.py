fb4946255fecd61c463406dcdf541f39a8e5169f
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-01-16 20:53:34 +0000

    bumped the compat version and zenpack version reviewed by jcausey
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.DeviceSearch@52975 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.DeviceSearch"
-VERSION = "1.1.0"
+VERSION = "1.2.0"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.DeviceSearch']
 INSTALL_REQUIRES = []
-COMPAT_ZENOSS_VERS = ">=3.0"
+COMPAT_ZENOSS_VERS = ">=4.1.0"
 PREV_ZENPACK_NAME = ""
 # STOP_REPLACEMENTS
 ################################
